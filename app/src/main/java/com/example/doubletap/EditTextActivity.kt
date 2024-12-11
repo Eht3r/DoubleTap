db0bf01e751e8cf81acc1151de51b3cdfefa3362
Mingyu <ww9986@hs.ac.kr>
2024-12-11 12:01:14 +0900

    주석 추가

--- app/src/main/java/com/example/doubletap/EditTextActivity.kt
+++ app/src/main/java/com/example/doubletap/EditTextActivity.kt
@@ -5,16 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.doubletap.databinding.ActivityEditTextBinding
 
 class EditTextActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEditTextBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_edit_text)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityEditTextBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
